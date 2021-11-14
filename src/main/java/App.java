513c03d6cdc77d332622fdfb40769dd9c5454d17
lokified <sheldon.obonyo@student.moringaschool.com>
2021-11-14 10:30:57 +0300

    add routes for departments: find, create and show all departments

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import Exceptions.APIException;
 import com.google.gson.Gson;
 import dao.Sql2oDepartmentDao;
 import dao.Sql2oUserDao;
+import models.Department;
 import models.User;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
@@ -20,6 +21,7 @@ public class App{
         String connectionString ="jdbc:postgresql://localhost:5432/mynews";
         Sql2o sql2o = new Sql2o(connectionString,"moringa","dammey5");
         Sql2oUserDao userDao = new Sql2oUserDao(sql2o);
+        Sql2oDepartmentDao departmentDao = new Sql2oDepartmentDao(sql2o);
         Connection conn = sql2o.open();
         Gson gson = new Gson();
 
@@ -29,6 +31,15 @@ public class App{
             userDao.add(user);
             res.status(201);
             return gson.toJson(user);
+        });
+
+        //create department
+        post("/departments/new","application/json",(req,res)->{
+            Department department = gson.fromJson(req.body(), Department.class);
+            departmentDao.add(department);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(department);
         });
 
 
@@ -53,6 +64,30 @@ public class App{
             return gson.toJson(foundUser);
         });
 
+        //get all departments
+        get("/departments","application/json",(req,res) ->{
+            if(departmentDao.getAll().size() > 0) {
+                return gson.toJson(departmentDao.getAll());
+            }
+            else {
+                return "no departments in the database";
+            }
+        });
+
+        //find departments
+        get("/departments/:id", "application/json", (req, res) -> {
+            int departmentId = Integer.parseInt(req.params("id"));
+
+            Department departmentToFind = departmentDao.findById(departmentId);
+
+            if (departmentToFind == null){
+                throw new APIException(404, String.format("No department with the id: \"%s\" exists", req.params("id")));
+            }
+
+            return gson.toJson(departmentToFind);
+        });
+
+        //FILTERS
         exception(APIException.class, (exception, req, res) -> {
             APIException err = (APIException) exception;
             Map<String, Object> jsonMap = new HashMap<>();
@@ -63,7 +98,7 @@ public class App{
             res.body(gson.toJson(jsonMap));
         });
 
-        //FILTERS
+
         after((req, res) -> {
             res.type("application/json");
         });
